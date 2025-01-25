1da9e9659505d40138536377f29cf60ef740b688
MD MEHEDI HASHAN MOLLA <mdmehedihashanmolla@gmail.com>
2025-01-25 21:15:36 +0530

    Adding Mapping Basics

---
+++ Mapping_Basics.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+
+contract AddressToInteger {
+    mapping(address => uint) public balances;
+
+    function setBalance(uint _value) public {
+        balances[msg.sender] = _value;
+    }
+
+    // Function to get the balance associated with an address
+
+    function getBalance(address _address) public view returns (uint) {
+        return balances[_address];
+    }
+
+    function updateBalance(uint _newValue) public {
+        balances[msg.sender] = _newValue;
+    }
+
+    function deleteBalance() public {
+        delete balances[msg.sender];
+    }
+}

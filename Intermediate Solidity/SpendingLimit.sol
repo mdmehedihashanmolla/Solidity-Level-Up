0c44488611b3143dd97bd99ddd30550abb2bd44e
MD MEHEDI HASHAN MOLLA <mdmehedihashanmolla@gmail.com>
2025-03-10 22:45:27 +0530

    init

--- Intermediate Solidity/SpendingLimit.sol
+++ Intermediate Solidity/SpendingLimit.sol
@@ -1,2 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0 <0.9.0;
+
+contract SpendingLimit {
+    address public owner;
+    uint public maxSpend;
+
+    constructor(uint _maxSpend) {
+        owner = msg.sender;
+        maxSpend = _maxSpend;
+    }
+    // Modifier : Restrict spending to maxSpend
+    modifier withinLimit(uint amount) {
+        require(amount <= maxSpend, "Transaction exceeds spending limit");
+        _;
+    }
+    function setMaxSpend(uint _newLimit) public {
+        require(msg.sender == owner, "Only owner can change limit!");
+        maxSpend = _newLimit;
+    }
+
+    function spend(uint amount) public withinLimit(amount) {
+        // Spending Logic
+    }
+}
